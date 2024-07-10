177eb0d36e1c891213d47b53391b0fc1a4448bfa
Milad Alkhamis <mi-alkhamis@hotmail.com>
2024-07-10 18:42:55 +0330

    style: remove docstings to increase code readability

--- main.py
+++ main.py
@@ -1,26 +1,23 @@
 import re
+import signal
+import sys
 from base64 import b64encode
 from datetime import date
-from ftplib import FTP as ftp, FTP_TLS as ftp_tls
+from ftplib import FTP as ftp
+from ftplib import FTP_TLS as ftp_tls
+from os import makedirs, path
 from uuid import uuid4
 from warnings import simplefilter
+
+import readchar
 import requests
-from urllib3.exceptions import InsecureRequestWarning
-from config import Config
-import signal, readchar, sys
-from os import path, makedirs
 from packaging import version
+from urllib3.exceptions import InsecureRequestWarning
+
+from config import Config
 
 
 def set_authorization_header():
-    """Create and return an authorization header for the API server.
-
-    The authorization header is based on the API key stored in the config module.
-    It uses the basic authentication scheme with base64 encoding.
-
-    Returns:
-        str: The authorization header value.
-    """
     login_string = f"{config.api_key}:"
     encoded_user_pass_sequence = str(b64encode(login_string.encode()), "utf-8")
     authorization_header = "Basic " + encoded_user_pass_sequence
@@ -28,18 +25,6 @@ def set_authorization_header():
 
 
 def get_package_name_list(authorization_header):
-    """Get and return a list of package names from the API server.
-
-    The package names are obtained by sending a JSON-RPC request to the API endpoint.
-    The request uses the authorization header and the getPackagesList method.
-    The package names are filtered to exclude the default security server package.
-
-    Args:
-        authorization_header (str): The authorization header value.
-
-    Returns:
-        list: A list of package names as strings.
-    """
     request_id = uuid4()
     api_endpoint = f"https://{config.api_server}/api/v1.0/jsonrpc/packages"
     data = f'{{"params": {{}},"jsonrpc": "2.0","method":"getPackagesList","id": "{request_id}"}}'
@@ -67,19 +52,6 @@ def get_package_name_list(authorization_header):
 
 
 def get_package_url(authorization_header, package_name):
-    """Get and return a list of package URLs from the API server.
-
-    The package URLs are obtained by sending a JSON-RPC request to the API endpoint.
-    The request uses the authorization header and the getInstallationLinks method.
-    The package URLs are filtered to include only the full kit windows links.
-
-    Args:
-        authorization_header (str): The authorization header value.
-        package_name (str): The name of the package.
-
-    Returns:
-        list: A list of package URLs as strings.
-    """
     request_id = uuid4()
     api_endpoint = f"https://{config.api_server}/api/v1.0/jsonrpc/packages"
     data = f'{{"params": {{"packageName":"{package_name}"}},"jsonrpc": "2.0","method": "getInstallationLinks","id": "{request_id}"}}'
@@ -106,23 +78,6 @@ def get_package_url(authorization_header, package_name):
 
 
 def get_package_file(authorization_header, package_url, package_group):
-    """Download a package file from the API server and upload it to the FTP server.
-
-    The package file is downloaded by sending a POST request to the package URL.
-    The request uses the authorization header and the Content-Type header.
-    The package file name and version are extracted from the response headers.
-    The package file is saved in a local directory named after the package group.
-    The package file is then uploaded to the FTP server using the send_to_ftp function.
-    The package file is only uploaded if the FTP server does not have a newer version.
-
-    Args:
-        authorization_header (str): The authorization header value.
-        package_url (str): The URL of the package file to download.
-        package_group (str): The name of the package group.
-
-    Returns:
-        bool: True if the FTP repository is up to date, False otherwise.
-    """
     try:
         result = requests.post(
             package_url,
@@ -153,18 +108,6 @@ def get_package_file(authorization_header, package_url, package_group):
 
 
 def send_to_ftp(package_group, package_filename, package_version):
-    """Upload a package file to the FTP server.
-
-    The package file is uploaded to the FTP server using the FTP class from the ftplib module.
-    The FTP server credentials and the root directory are obtained from the config module.
-    The package file is stored in a subdirectory named after the package group.
-    The FTP connection is closed after the upload is completed.
-
-    Args:
-        package_group (str): The name of the package group.
-        package_filename (str): The path of the package file to upload.
-        package_version (str): The version of the package file.
-    """
     ftp_user = config.ftp_user
     ftp_password = config.ftp_password
     ftp_host = config.ftp_server
@@ -181,21 +124,6 @@ def send_to_ftp(package_group, package_filename, package_version):
 
 
 def check_packages_version(package_group, bit_version):
-    """Check the package version on the FTP server and delete older files.
-
-    The package version is compared with the existing files on the FTP server using the re module.
-    The FTP server credentials and the root directory are obtained from the config module.
-    The package files are listed in a subdirectory named after the package group.
-    If the package version is newer than the existing files, the older files are deleted.
-    If the package version is equal to or older than the existing files, the function returns True.
-
-    Args:
-        package_group (str): The name of the package group.
-        version (str): The version of the package file.
-
-    Returns:
-        bool: True if the FTP server has a newer or equal version, False otherwise.
-    """
     ftp_user = config.ftp_user
     ftp_password = config.ftp_password
     ftp_host = config.ftp_server
@@ -218,16 +146,6 @@ def check_packages_version(package_group, bit_version):
 
 
 def exit_handler(signum, frame):
-    """
-    Handle the exit signal by asking the user for confirmation.
-
-    Args:
-        signum (int): The signal number.
-        frame (frame object): The current stack frame.
-
-    Returns:
-        None
-    """
     messsage = "Ctrl+C was pressed. Do you really want to exit? y/n "
     print(messsage, end="", flush=True)
     respond = readchar.readchar()
